81a4f48a47142343dd4fee617472242129354f2a
jharris4 <harris.jb@gmail.com>
2016-01-18 16:43:35 -0500

    Refactor the suite loader to use the script loader to avoid requiring hacky webpack configs, and remove the decorate query option

--- LoaderMocha.js
+++ LoaderMocha.js
@@ -1,13 +1,14 @@
-var Mocha = require('mocha/lib/mocha.js');
-
 function LoaderMocha(options) {
     this.options = options || {};
     if (!this.options.ui) {
         this.options.ui = "bdd";
     }
     this.context = {};
-    this.mocha = new Mocha({ reporter: function() {} });
-    this.mocha.ui(this.options.ui);
+
+    require('!!script!mocha/mocha.js');
+
+    var Mocha = window.Mocha;
+    var mocha = this.mocha = new Mocha({ ui: this.options.ui, reporter: function() {} });
     this.mocha.suite.emit('pre-require', this.context, null, this.mocha);
 }
 
@@ -21,7 +22,7 @@ LoaderMocha.prototype.setupTestContext = function(global) {
 
 LoaderMocha.prototype.suite = function() {
     return this.mocha.suite;
-}
+};
 
 LoaderMocha.prototype.teardownTestContext = function(global) {
     if (this.context) {
--- index.js
+++ index.js
@@ -10,21 +10,15 @@ module.exports.pitch = function(req) {
 	this.cacheable && this.cacheable();
 	var query = loaderUtils.parseQuery(this.query);
     var ui = JSON.stringify(query["interface"] || "bdd");
-    var decorate = query["decorate"] === 'true';
 	var source = [];
 	if(this.target == "web") {
 		source.push("var LoaderMocha = require(" + JSON.stringify(path.join(__dirname, "LoaderMocha.js")) + ");");
-		source.push("var loaderMocha = new LoaderMocha({ ui: " + ui + "})");
+		source.push("var loaderMocha = new LoaderMocha({ ui: " + ui + " })");
 		source.push("loaderMocha.setupTestContext(window);");
 		source.push("require(" + JSON.stringify("!!" + req) + ")");
 		source.push("loaderMocha.teardownTestContext(window);");
         source.push("var suite = loaderMocha.suite();");
-        if (decorate) {
-            source.push("suite.module = {};");
-            source.push("suite.module.id = " + module.id + ";");
-            source.push("suite.module.file = " + this.resource + ";");
-        }
-		source.push("var exports = module.exports = suite;");
+        source.push("var exports = module.exports = suite;");
 
 	} else {
 		throw new Error("Unsupported target environment " + this.target);
